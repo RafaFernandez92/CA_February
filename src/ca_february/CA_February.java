ef8345fd177a4f5a3441c9be967789a7eb9c52e1
RafaFernandez92 <152530730+RafaFernandez92@users.noreply.github.com>
2024-01-17 14:48:48 +0000

    Created file
    Created file for Github and testing it with a simple print

--- src/ca_february/CA_February.java
+++ src/ca_february/CA_February.java
@@ -15,6 +15,10 @@ public class CA_February {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hola hola!"); 
+        
+        
     }
     
 }
