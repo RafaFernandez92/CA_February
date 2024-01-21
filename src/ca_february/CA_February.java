3100e3c1d4186149ef795367ad336b8ba7621bb7
RafaFernandez92 <152530730+RafaFernandez92@users.noreply.github.com>
2024-01-21 02:24:52 +0000

    Still trying to get right Scanner file reading

--- src/ca_february/CA_February.java
+++ src/ca_february/CA_February.java
@@ -4,6 +4,7 @@
  */
 package ca_february;
 
+import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
 import java.util.*;
@@ -19,13 +20,18 @@ public class CA_February {
      */
     public static void main(String[] args) {
     
-    File fileobj = new File("C:\Users\User\OneDrive\Desktop\status.txt");
-    Scanner input = new Scanner(fileobj);
+//    File fileobj = new File("C:\Users\User\OneDrive\Desktop\status.txt");
+//    Scanner in = new Scanner(fileobj);
     
-    while(input.hasNext())
-    {
-        String a = input.nextLine();
-        System.out.println(a);
+    try{
+        Scanner statusFile = new Scanner(new FileReader("C:\\Users\\User\\OneDrive\\Documentos\\NetBeansProjects\\CA_February\\status.txt"));
+        someLine = statusFile.nextLine();
+        
+        System.out.println(someLine);
+        
+    }catch (Exception e) {
+        System.out.println("File error");
+    }
     }
         
         
